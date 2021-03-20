c66e2a429de8154edf9de6402454e1600dbfe1f7
Carrilhos <gcarrilhos@gmail.comm>
2021-03-20 16:57:38 -0300

    testando

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,50 @@
-import '@styles/globals.css'
+import React from 'react'
+import { createGlobalStyle } from 'styled-components'
+import * as Sentry from '@sentry/node'
 
-function Application({ Component, pageProps }) {
-  return <Component {...pageProps} />
+import PageWrapper from 'components/PageWrapper'
+
+import AppProviders from 'context'
+
+import Theme from 'theme'
+
+import { version, name } from '../package.json'
+
+const GlobalStyle = createGlobalStyle`
+* {
+  border: 0;
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+  outline: none;
 }
 
-export default Application
+button, a {
+  cursor: pointer;
+  &:disabled{
+    cursor: not-allowed;
+  }
+}
+`
+
+if (process.env.NEXT_PUBLIC_NODE_ENV === 'production') {
+  Sentry.init({
+    dsn: process.env.NEXT_PUBLIC_SENTRY_URL,
+    environment: process.env.NEXT_PUBLIC_NODE_ENV,
+    debug: process.env.NEXT_PUBLIC_NODE_ENV !== 'production',
+    release: `${name}@${version}`
+  })
+}
+
+const App = ({ Component, pageProps }) => (
+  <AppProviders>
+    <GlobalStyle />
+    <Theme>
+      <PageWrapper>
+        <Component {...pageProps} />
+      </PageWrapper>
+    </Theme>
+  </AppProviders>
+)
+
+export default App
